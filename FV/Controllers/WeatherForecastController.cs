8603e3d4d048eb4e5596426d09222d715a900fce
Diogo Namie <Diogo.Keiti@libertyseguros.com.br>
2019-12-24 21:24:07 -0300

    Adicionado.

--- FV/Controllers/WeatherForecastController.cs
+++ FV/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace FV.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Brazil"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
